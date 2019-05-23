bd7bac138d27eccc3c63ab230f12c4f7ce8e9534
HIROSE Masaaki <hirose31@gmail.com>
2019-05-23 20:31:50 +0900

    1

--- ooo.py
+++ ooo.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
+
 import pprint
 from functools import partial
 p = partial(pprint.pprint, width=1)
@@ -9,6 +11,9 @@ p = partial(pprint.pprint, width=1)
 def main():
     print('Hello')
     p({'foo': 'F', 'bar': 'B'})
+
+
+
 
 
 if __name__ == '__main__':
