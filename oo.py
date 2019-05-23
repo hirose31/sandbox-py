9e8549af9b8cbdc4d9997dd5508982e271a1b4f3
HIROSE Masaaki <hirose31@gmail.com>
2019-05-23 21:21:20 +0900

    More

--- oo.py
+++ oo.py
@@ -8,12 +8,31 @@ from functools import partial
 p = partial(pprint.pprint, width=1)
 
 
+def foo():
+    print('foo')
+
+
+
+
 def main():
-    print('Hello')
+    print('Hello') #E262!
     p({'foo': 'F', 'bar':'B'})
+    print("Python", ("Hello",
+                "World"))
+    x = 6
+    y = 1
+    if x > 5: y = 10
+
+    z = (x +
+         y)
+
+    my_tuple = 1,	2,	3 # E242
 
 
 
+
+#E265!
+## E266!
 
 
 if __name__ == '__main__':
